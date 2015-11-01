a392ac89cac19bb356998604c7983ac3deeb3eaa
advcodelrn <advstud@mail.com>
2015-11-01 21:06:59 +0000

    Completed part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -24,15 +24,21 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s = s.to_s
+  if s =~ /\A[BCDFGHJKLMNPQRSTVXZWY]/i 
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /\A[01]+\Z/
+  s.to_i(2) % 4 == 0
 end
 
 # Part 3
